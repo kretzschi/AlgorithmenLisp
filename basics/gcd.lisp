a25153fe777678cb7c05e4e07cce593198a6d9fb
kretzschi <ksascha89@hotmail.de>
2015-01-20 01:17:54 +0100

    minor bug fix

--- basics/gcd.lisp
+++ basics/gcd.lisp
@@ -2,4 +2,4 @@
 ;; Inspired by the pseudo code of https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations
 (defun gcd (firstNumber secondNumber)
   (if (zerop secondNumber) firstNumber 
-      (gcd2 secondNumber (mod firstNumber secondNumber))))
+      (gcd secondNumber (mod firstNumber secondNumber))))
